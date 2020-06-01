f5f5060218bd4582377fb9af4ab481978f429dbf
Eumie Jhong <40900105+eumiejhong@users.noreply.github.com>
2020-05-31 20:45:13 -0500

    Add files via upload

---
+++ gather_examples.py
@@ -0,0 +1,41 @@
+#Usage
+# python gather_examples.py --input videos/real.mov --output dataset/real --detector face_detector --skip 1
+# python gather_examples.py --input videos/fake.mp4 --output dataset/fake --detector face_detector --skip 1
+
+# import the necessary package from imutils.video import VideoStream
+import imutils
+import numpy as np
+import argparse
+import cv2
+import os
+import os.path
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, required=True,
+	help="path to input video")
+ap.add_argument("-o", "--output", type=str, required=True,
+	help="path to output directory of cropped faces")
+args = vars(ap.parse_args())
+
+
+vs = cv2.VideoCapture(args["input"])
+currentframe = 0
+
+while(True):
+    #reading from frame
+    ret, frame = vs.read()
+
+    if ret:
+        name = "output" + str(currentframe) + '.png'
+        print ('Creating...' + name)
+
+        fileName = os.path.join(args["output"], name)
+        cv2.imwrite(fileName, frame)
+        currentframe += 1
+    else:
+        break
+
+#cleanup
+vs.release()
+cv2.destroyAllWindows()
